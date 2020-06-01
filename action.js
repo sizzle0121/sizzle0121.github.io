7d700dcfcdfc26ee87f214204ebde3161b97be33
Sizzle Hsu <sizzle0121.cs04@nctu.edu.tw>
2020-06-02 01:13:36 +0800

    generally completed

---
+++ action.js
@@ -0,0 +1,44 @@
+
+
+function SkillsEnter(){
+	var sec = document.getElementById("Skills");
+	var scrollY = window.pageYOffset;
+	var scrollUnit = sec.scrollHeight;
+
+	//console.log(scrollY);
+	//console.log(scrollUnit);
+
+	if(scrollY > 2.6*scrollUnit){
+		var ids = ["#Skills-bar-python-done", "#Skills-bar-DRL-done", "#Skills-bar-team-done", "#Skills-bar-c-done", "#Skills-bar-algo-done", "#Skills-bar-math-done"];
+		setTimeout(() => {
+			for(var i=0; i<3; i++){
+				const skill = document.querySelector(ids[i]);
+				skill.style.width = skill.getAttribute('data-done') + '%';
+				skill.style.opacity = 1;
+			}
+		}, 100);
+		setTimeout(() => {
+			for(var i=3; i<6; i++){
+				const skill = document.querySelector(ids[i]);
+				skill.style.width = skill.getAttribute('data-done') + '%';
+				skill.style.opacity = 1;
+			}
+		}, 1000);
+	}
+}
+
+window.addEventListener("scroll", SkillsEnter);
+
+
+function toggleSideBar(){
+	document.querySelector('#side-bar').classList.toggle('side-bar-active');
+	document.querySelector('#main-container').classList.toggle('main-container-active');
+	document.querySelector('#particles-js').classList.toggle('particles-js-active');
+	if(document.querySelector('.fa-chevron-right')){
+		document.querySelector('#toggle-button-icon').classList.remove('fa-chevron-right');
+		document.querySelector('#toggle-button-icon').classList.add('fa-chevron-left');
+	}else if(document.querySelector('.fa-chevron-left')){
+		document.querySelector('#toggle-button-icon').classList.remove('fa-chevron-left');
+		document.querySelector('#toggle-button-icon').classList.add('fa-chevron-right');
+	}
+}
